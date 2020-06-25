77659a71fd46e8f8273c06a2ca0600bbfa89c7f1
Sam Thorogood <sam.thorogood@gmail.com>
2020-06-25 23:53:50 +1000

    lint

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -9,7 +9,9 @@ import {store} from './store';
 import {normalizeUrl} from './urls';
 import './utils/underscore-import-polyfill';
 
-const pageResourcesVersion = document.body.getAttribute('data-resources-version');
+const pageResourcesVersion = document.body.getAttribute(
+  'data-resources-version',
+);
 
 /**
  * Dynamically loads code required for the passed URL entrypoint.
